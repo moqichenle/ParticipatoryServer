ab7df4fd22c589ff6a3a3876f82186224dcf9422
Lele <lihe@tcd.ie>
2013-07-01 19:10:06 +0100

    get addNewUser request from client and add new user in database

--- src/ie/tcd/scss/dsg/test/TestUser.java
+++ src/ie/tcd/scss/dsg/test/TestUser.java
@@ -4,6 +4,7 @@ import ie.tcd.scss.dsg.particpatory.StorageManager;
 import ie.tcd.scss.dsg.po.Location;
 import ie.tcd.scss.dsg.po.User;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 
 public class TestUser extends HttpServlet {
 
@@ -44,6 +46,30 @@ public class TestUser extends HttpServlet {
 		StorageManager sm = new StorageManager();
 		user = (User) sm.add(user);
 		resp.getWriter().write(user.getUserId().toString());
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		if (req.getContentType().equals("application/json")) {
+			System.out.println("get json request");
+			try {
+				BufferedReader reader = req.getReader();
+				Gson gson = new Gson();
+				User user = gson.fromJson(reader, User.class);
+				System.out.println("get user ^-^");
+				StorageManager sm = new StorageManager();
+				user = (User) sm.add(user);
+				System.out.println("userId is:"+user.getUserId());
+				resp.getWriter().write(user.getUserId().toString());
+			} catch (Exception e) {
+				// crash and burn
+				throw new IOException("Error parsing JSON request string");
+			}
+		}else{
+			resp.setStatus(500);
+		}
+		
 	}
 
 }
